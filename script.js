b8be3c68bd71136dc3a69edcade49a9bcecb0215
AlbertLindhjemEngedal <albert.engedal@voca.no>
2025-02-14 10:23:03 +0100

    asset pack import

---
+++ script.js
@@ -0,0 +1,27 @@
+
+class Platform {
+
+    constructor(width, height, type, x, y) {
+        this.width = width;
+        this.height = height;
+        this.type = type;
+        this.x = x;
+        this.y = y;
+    }
+    
+    Create() {
+       for(i = 0; i < this.width; i++) {
+
+        if i == 0 {
+            
+        }
+
+
+       }
+
+    }
+}
+
+const platform = new Platform(100, 100, "grass", 0, 0);
+
+platform.Create()
